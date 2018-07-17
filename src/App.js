6f6a9933df3d6d024d8889e2b4d3bba2adad1603
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-07-16 22:31:53 -0400

    add basic routing to the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
+const App = () => (
+  <Router>
+    <div>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Tech Glossary</h1>
         </header>
-
-        <Button variant="fab" color="primary" aria-label="Add">
-          <AddIcon />
-        </Button>
-
-        <p className="App-intro">
-          There's nothing here, add a term!
-        </p>
       </div>
-    );
-  }
-}
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      {/* <Route path="/topics" component={Topics} /> */}
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div className="App">
+    <Link to="/about">
+      <Button variant="fab" color="primary" aria-label="Add">
+        <AddIcon />
+      </Button>
+    </Link>
+
+    <p className="App-intro">
+      There's nothing here, add a term!
+    </p>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>Insert Add page here</h2>
+  </div>
+);
+
+// const Topics = ({ match }) => (
+//   <div>
+//     <h2>Topics</h2>
+//     <ul>
+//       <li>
+//         <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/components`}>Components</Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+//       </li>
+//     </ul>
+
+//     <Route path={`${match.url}/:topicId`} component={Topic} />
+//     <Route
+//       exact
+//       path={match.url}
+//       render={() => <h3>Please select a topic.</h3>}
+//     />
+//   </div>
+// );
+
+// const Topic = ({ match }) => (
+//   <div>
+//     <h3>{match.params.topicId}</h3>
+//   </div>
+// );
 
 export default App;
