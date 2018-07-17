c776ffc97cf5be0447fab62da58dafc6ea1831cb
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-07-16 20:38:15 -0400

    update font for correct weight and color

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Tech Glossary</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          There's nothing here, add a term!
         </p>
       </div>
     );
