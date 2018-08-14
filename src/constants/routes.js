a9839ec77285687fb6a257ab403b7e3eb126ecac
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-08-14 19:10:59 -0400

    add other routes to constants file

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,2 +1,6 @@
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
 export const LANDING = '/';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
 export const ENTER_TERM = '/enterterm';
