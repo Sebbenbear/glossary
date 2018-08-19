f2762bdbc9a8c695baa3f145b29ab295e4c00307
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-08-18 21:42:25 -0400

    Change landing page to use style

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+const style = {
+  marginLeft: '20px'
+}
+
 const LandingPage = () =>
-  <div>
-    <h1>Landing Page</h1>
+  <div style={style}>
+    <h1>Welcome to Glossary!</h1>
   </div>
 
 export default LandingPage;
